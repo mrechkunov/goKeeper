7fde6ce7ec469f9da5046c5028a5743035ac3ea4
Michael Rechkunov <mrechkunov@gmail.com>
2026-07-06 16:29:50 +0300

    add CRUD operations for users table

--- internal/repository/storage_users.go
+++ internal/repository/storage_users.go
@@ -1,10 +1,14 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
+	"time"
 
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/mrechkunov/goKeeper.git/internal/logger"
+	"github.com/mrechkunov/goKeeper.git/internal/model"
 )
 
 type StorageUsers struct {
@@ -17,11 +21,64 @@ func NewUsersStorage(DBconn *sql.DB) StorageUsers {
 }
 
 // Close DB connection
-func (d *StorageUsers) Close() error {
-	return d.DBconnection.Close()
+func (su *StorageUsers) Close() error {
+	return su.DBconnection.Close()
 }
 
 // добавить пользователя C
-// авторизовать пользователя (проверить что он есть и вернуть uuid) R
-// изменить данные пользователя (только если uuid верный) U
+func (su *StorageUsers) CreateUser(ctx context.Context, user model.Users) error {
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	sqlStatement := `INSERT INTO users (u_login, u_password, u_token) 
+				VALUES ($1, $2, $3)`
+	_, err := su.DBconnection.ExecContext(ctxWithTimeout, sqlStatement, user.Login, user.Password, user.Token)
+	if err != nil {
+		logger.Log.Errorln("error while insert user to DB", err)
+		return err
+	}
+	return nil
+}
+
+// авторизовать пользователя (проверить что он есть и вернуть token) R
+func (su *StorageUsers) ReadUser(ctx context.Context, login string) (user model.Users, err error) {
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	sqlStatement := `SELECT u_login, u_password, u_token FROM users WHERE u_login = $1;`
+	err = su.DBconnection.QueryRowContext(ctxWithTimeout, sqlStatement, login).Scan(&user.Login, &user.Password, &user.Token)
+	if err == sql.ErrNoRows {
+		logger.Log.Infoln("user with login: ", login, "is not exist in DB")
+		return user, err
+	}
+	return user, nil
+}
+
+// изменить данные пользователя (только если token верный) U
+func (su *StorageUsers) UpdateUser(ctx context.Context, user model.Users) error {
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	sqlStatement := `UPDATE users 
+				SET u_password = $1, 
+				u_token = $2
+				WHERE u_login = $3;`
+	_, err := su.DBconnection.ExecContext(ctxWithTimeout, sqlStatement, user.Password, user.Token, user.Login)
+	if err != nil {
+		logger.Log.Errorln("error while update user data in DB", err)
+		return err
+	}
+	return nil
+}
+
 // удалить пользователя и все его данные D
+func (su *StorageUsers) DeleteUser(ctx context.Context, user model.Users) error {
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	sqlStatement := `DELETE FROM users
+				WHERE u_login = $1;`
+	_, err := su.DBconnection.ExecContext(ctxWithTimeout, sqlStatement, user.Login)
+	if err != nil {
+		logger.Log.Errorln("error while delete user in DB", err)
+		return err
+	}
+	//TODO: вызвать функцию удаления данных в таблицах паролей, карт, бинарников в 3-х горутиранх с контекстом
+	return nil
+}
